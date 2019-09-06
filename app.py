d700b66769a1a5bde52cffc07da810958d23f7fe
LeoKnaw <43099122+LeoKnaw@users.noreply.github.com>
2019-09-06 13:54:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
 	new_name = str(name) + " shot the sheriff"
-	return render_template('index.html', name=new_name)
+	return render_template('index.html')
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
